d7e4ba640b75ee71141dec5f6469aa77cd1d08d5
Valentin Faribeault <vfaribeault@outlook.com>
2021-05-11 15:25:05 +0200

    Update on JS and CSS files

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,26 +1,34 @@
 // Import ShoppingList.css file to add style
 import '../styles/ShoppingList.css'
-
-// Create a plantList array
-const plantList = [
-  'monstera',
-  'ficus lyrata',
-  'pothos argenté',
-  'yucca',
-  'palmier'
-]
+// Import plantList array
+import { plantList } from '../datas/plantList'
 
 // Create a ShoppingList component
 function ShoppingList() {
-  // Iterate on the plantList array to generate each article separately and its key
-  return (
-    <ul>
-      {plantList.map((plant, index) => (
-        // Do not miss to generate each element key with the index
-        <li key={`${plant}-${index}`}>{ plant }</li>
-      ))}
-    </ul>
-  )
+  // Use an accumalator to try to reduce each list value to one value
+	const categoryList = plantList.reduce((accumulator, plant) =>
+    // If category already exists in accumulator, send back accumulator and go to next value
+    // If not, push the category inside a new accumulator array
+    accumulator.includes(plant.category) ? accumulator : accumulator.concat(plant.category),
+		[]
+	)
+  // Iterate on categoryList and plantList
+	return (
+		<div>
+			<ul>
+				{categoryList.map((category) =>
+          // Do not miss to generate each element key with data value
+					<li key={category}>{category}</li>
+				)}
+			</ul>
+			<ul>
+				{plantList.map((plant) =>
+          // Do not miss to generate each element key with id
+					<li key={plant.id}>{plant.name}</li>
+				)}
+			</ul>
+		</div>
+	)
 }
 
 // Export file to import it in "App.js"
