fcec47acc7d78aa399b81d5df5358a356917d4f0
Valentin Faribeault <vfaribeault@outlook.com>
2021-05-20 13:24:16 +0200

    Second commit

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,10 +3,16 @@ import CareScale from './CareScale'
 // Import PlantItem.css file to add style
 import '../styles/PlantItem.css'
 
+// Declare a function to call when you'll ckick on an item
+function handleClick(e) {
+	alert(`Vous voulez acheter 1 ${e} ? Très bon choix 🌱✨`)
+}
+
 // PlanItem component takes 5 props
 function PlantItem({ name, cover, id, light, water }) {
   return (
-		<li key={id} className='tjh-plant-item'>
+		// Declare an 'onClick' event, pass it 'handleClick' function with the name as argument
+		<li key={id} className='tjh-plant-item' onClick={() => handleClick(name)}>
 			<img className='tjh-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
