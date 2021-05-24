8c915e7cb955b827407875cfdf431b1f6ea71c95
Valentin Faribeault <vfaribeault@outlook.com>
2021-05-24 15:43:06 +0200

    Cart stateful component added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,27 @@
+// Import local state from react to save informations
+import { useState } from 'react'
 // Import Cart.css file to add style
 import '../styles/Cart.css'
 
 // Create a Cart component
 function Cart() {
-  // Create variables for each price
-  const priceMonstera = 8
-  const priceIvy = 10
-  const priceBunchOfFlowers = 15
+  // Create variable for the monstera price
+  const monsteraPrice = 8
+  // Create a Cart stateful component init with 0€
+  // "cart" is the init variable and "updateCart" the setting function
+  const [cart, updateCart] = useState(0)
+
   // Add className on cart to then manage style
   return (
     <div className='tjh-cart'>
-      <h2>My cart</h2>
-        <ul>
-          <li>Monstera : { priceMonstera }€</li>
-          <li>Ivy : { priceIvy }€</li>
-          <li>Bunch of flowers : { priceBunchOfFlowers }€</li>
-        </ul>
-        <p>Total price : { priceMonstera + priceIvy + priceBunchOfFlowers }€</p>
+      <h2>Panier</h2>
+      <div>
+        Monstera : {monsteraPrice}€
+        <button onClick={() => updateCart(cart + 1)}>
+          Ajouter
+        </button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
     </div>
   )
 }
