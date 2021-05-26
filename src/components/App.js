5427ba8d25386c8251ad5c94614a6d2c6ccaeb8f
Valentin Faribeault <vfaribeault@outlook.com>
2021-05-26 14:40:48 +0200

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,22 @@ import '../styles/Layout.css'
 
 // Create an App component
 function App() {
+  // Create a Cart state variable, with "cart" as init variable and "updateCart" as setting function
+  const [cart, updateCart] = useState([])
+
+
   return (
     <div>
+      
       <Banner />
+      
       <div className='tjh-layout-inner'>
         <Cart />
         <ShoppingList />
       </div>
+      
       <Footer />
+    
     </div>
   )
 }
